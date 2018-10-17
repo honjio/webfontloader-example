a6967bd13d5ea38f565ea32064d780cc04de4a31
fujino-yuki <xxx153yuu.story@gmail.com>
2018-10-17 12:00:59 +0900

    first-commit

---
+++ main.js
@@ -0,0 +1,25 @@
+import WebFont from 'webfontloader';
+
+WebFont.load({
+    google: {
+        families: ['Roboto Condensed']
+    },
+    loading: function() {
+        console.log('loading');
+    },
+    active: function() {
+        console.log('active');
+    },
+    inactive: function() {
+        console.log('inactive');
+    },
+    fontloading: function(familyName, fvd) {
+        console.log('fontloading', familyName, fvd);
+    },
+    fontactive: function(familyName, fvd) {
+        console.log('fontactive', familyName, fvd);
+    },
+    fontinactive: function(familyName, fvd) {
+        console.log('fontinactive', familyName, fvd);
+    }
+});
